05d66b3e432594c4ec01c6ba4dae5083ce9e68c8
Vitaly Pronkin <pronvit@me.com>
2016-06-16 01:48:52 +1200

    written objects count and dance floor size for locations

--- lua/locations.lua
+++ lua/locations.lua
@@ -74,101 +74,104 @@ local occupation_names = {
 }
 
 function location_get_info(id)
-	local loc = location_find_by_id(id)
-	if not loc then
-		error('no location '..tostring(id))
-	end
+	return execute_with_locations_screen(function(ws)
+		for j,loc in ipairs(ws.locations) do
+			if loc and loc.id == id then
+				local ltype = loc:getType()
 
-	local ltype = loc:getType()
+				local info
+				local params
+				if ltype == df.abstract_building_type.LIBRARY then
+					local count_written = ws.anon_1[j]
+					local count_paper = loc.contents.unk_100
 
-	local info
-	local params
-	if ltype == df.abstract_building_type.LIBRARY then
-		local count_written = 99 --todo: this
-		local count_paper = loc.contents.unk_100
+					info = {
+						count_buildings(loc, df.building_type.Bookcase), count_written,
+						count_buildings(loc, df.building_type.Box), count_paper,
+						count_buildings(loc, df.building_type.Table), count_buildings(loc, df.building_type.Chair)
+					}
 
-		info = {
-			count_buildings(loc, df.building_type.Bookcase), count_written,
-			count_buildings(loc, df.building_type.Box), count_paper,
-			count_buildings(loc, df.building_type.Table), count_buildings(loc, df.building_type.Chair)
-		}
+					params = { loc.contents.desired_copies, loc.contents.desired_paper }
 
-		params = { loc.contents.desired_copies, loc.contents.desired_paper }
+				elseif ltype == df.abstract_building_type.INN_TAVERN then
+					local count_goblets = loc.contents.unk_f8
+					local count_instruments = loc.contents.unk_fc
+					local dance_area = { ws.dance_floor_x[j], ws.dance_floor_y[j] }
 
-	elseif ltype == df.abstract_building_type.INN_TAVERN then
-		local count_goblets = loc.contents.unk_f8
-		local count_instruments = loc.contents.unk_fc
-		local dance_area = { 1,1 } --todo: this
+					local rented_rooms = 0
+					local total_rooms = 0
+					
+					for i,v in ipairs(loc.contents.unk_108) do
+						local bld = df.building.find(v)
 
-		local rented_rooms = 0
-		local total_rooms = 0
-		
-		for i,v in ipairs(loc.contents.unk_108) do
-			local bld = df.building.find(v)
+						if bld then
+							local btype = bld:getType()
+							if btype == df.building_type.Bed then
+								total_rooms = total_rooms + 1
+								if bld.owner_id ~= -1 then
+									rented_rooms = rented_rooms + 1
+								end
+							end
+						end
+					end		
 
-			if bld then
-				local btype = bld:getType()
-				if btype == df.building_type.Bed then
-					total_rooms = total_rooms + 1
-					if bld.owner_id ~= -1 then
-						rented_rooms = rented_rooms + 1
+					info = {
+						count_buildings(loc, df.building_type.Box),
+						count_goblets, count_instruments,
+						rented_rooms, total_rooms,
+						dance_area,
+					}
+
+					params = { loc.contents.desired_goblets, loc.contents.desired_instruments }
+
+				elseif ltype == df.abstract_building_type.TEMPLE then
+					local count_instruments = loc.contents.unk_fc
+					local dance_area = { ws.dance_floor_x[j], ws.dance_floor_y[j] }
+
+					local deity = loc.deity ~= -1 and df.historical_figure.find(loc.deity)
+					local deity_name = deity and hfname(deity, true) or mp.NIL
+
+					info = {
+						count_buildings(loc, df.building_type.Box), count_instruments,
+						dance_area, deity_name,
+					}
+
+					params = { loc.contents.desired_instruments }
+				end
+
+				local occupations = {}
+				for i,occ in ipairs(loc.occupations) do
+					local unit = occ.unit_id ~= -1 and df.unit.find(occ.unit_id) or nil
+					local unitname = occ.unit_id ~= -1 and (unit and unit_fulltitle(unit) or '#unknown unit#') or mp.NIL
+
+					-- grouping by occupation type, and not allowing >1 unassigned occupation of one type
+					local pos = #occupations + 1
+					for j,v in ipairs(occupations) do
+						if v[5] == -1 and occ.type == v[3] then
+							pos = occ.unit_id == -1 and 0 or j
+							break
+						end
+						if v[3] > occ.type then
+							pos = j
+							break
+						end
+					end
+
+					if pos > 0 then
+						table.insert(occupations, pos, { occupation_names[occ.type+1], occ.id, occ.type, unitname, occ.unit_id })
 					end
 				end
-			end
-		end		
 
-		info = {
-			count_buildings(loc, df.building_type.Box),
-			count_goblets, count_instruments,
-			rented_rooms, total_rooms,
-			dance_area,
-		}
+				local allow_residents = loc.flags[5]
+				local allow_outsiders = loc.flags[4]
+				local mode = allow_outsiders and 2 or (allow_residents and 1 or 0)
 
-		params = { loc.contents.desired_goblets, loc.contents.desired_instruments }
-
-	elseif ltype == df.abstract_building_type.TEMPLE then
-		local count_instruments = loc.contents.unk_fc
-		local dance_area = { 1,1 } --todo: this
-
-		local deity = loc.deity ~= -1 and df.historical_figure.find(loc.deity)
-		local deity_name = deity and hfname(deity, true) or mp.NIL
-
-		info = {
-			count_buildings(loc, df.building_type.Box), count_instruments,
-			dance_area, deity_name,
-		}
-
-		params = { loc.contents.desired_instruments }
-	end
-
-	local occupations = {}
-	for i,occ in ipairs(loc.occupations) do
-		local unit = occ.unit_id ~= -1 and df.unit.find(occ.unit_id) or nil
-		local unitname = occ.unit_id ~= -1 and (unit and unit_fulltitle(unit) or '#unknown unit#') or mp.NIL
-
-		-- grouping by occupation type, and not allowing >1 unassigned occupation of one type
-		local pos = #occupations + 1
-		for j,v in ipairs(occupations) do
-			if v[5] == -1 and occ.type == v[3] then
-				pos = occ.unit_id == -1 and 0 or j
-				break
-			end
-			if v[3] > occ.type then
-				pos = j
-				break
+				return { locname(loc), loc.id, ltype, mode, info, occupations, params }
 			end
 		end
 
-		if pos > 0 then
-			table.insert(occupations, pos, { occupation_names[occ.type+1], occ.id, occ.type, unitname, occ.unit_id })
-		end
-	end
-
-	local allow_residents = loc.flags[5]
-	local allow_outsiders = loc.flags[4]
-	local mode = allow_outsiders and 2 or (allow_residents and 1 or 0)
-
-	return { locname(loc), loc.id, ltype, mode, info, occupations, params }
+		error('no location '..tostring(id))		
+	end)
 end
 
 function location_occupation_get_candidates(locid, occid)
