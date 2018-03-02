a325001c713b5a44eab2d2d0eff63c990e564ef7
Vitaly <vit@ubuntu-512mb-sfo1-01.localdomain>
2018-03-02 00:22:21 +0000

    some changes for dfaas

---
+++ lua/dfaas.lua
@@ -0,0 +1,29 @@
+--luacheck: in=string,string
+local function dfaas_get_status(clientver, pwd)
+    local idle_time = os.time() - STATE.last_cmd_time
+    
+    return { setup_get_server_info(clientver, pwd), idle_time }
+end
+
+--luacheck: in=string
+function dfaas_save_game(pwd)
+    if not native.verify_pwd(pwd or '') then
+        error('invalid password')
+    end
+
+    --todo: need to return to main screen!
+
+    save_game()    
+    return true
+end
+
+--luacheck: in=string
+function dfaas_save_done(pwd)
+    if not native.verify_pwd(pwd or '') then
+        error('invalid password')
+    end
+
+    --todo: need to return to main screen!
+
+    return (df.global.ui.main.autosave_request ~= true)
+end
