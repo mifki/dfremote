955a2ecce88cded8c1b9ced3c537e8c2ff21f8d3
Vitaly <vit@ubuntu-512mb-sfo1-01.localdomain>
2018-11-01 22:25:34 +1300

    tweaking corehacks

--- corehacks.hpp
+++ corehacks.hpp
@@ -8,15 +8,12 @@ void core_suspend_fast()
     // Claim channel lock early to make sure no commands posted
     SDL_SemWait(enabler->async_tobox.sem);
 
-    *paused = true;
-
     // If async_frames is zero, mainloop() has already finished or wasn't requested.
-    // And since we've already set async_paused=true, mainloop() won't be called even if requested.
     // So we're safe to proceed in either case.
     if (!*frames)
     {
-        saved_frames = 0;
         *paused = true;
+        saved_frames = 0;
         return;
     }
 
@@ -35,33 +32,6 @@ void core_suspend_fast()
     } while (!*paused);
 
     saved_frames = *frames;
-    /* *frames = 0;
-
-    // Trigger async_frames change
-    df::enabler::T_async_tobox::T_queue command;
-    command.cmd = df::enabler::T_async_tobox::T_queue::inc;
-    command.val = 99999;
-    enabler->async_tobox.queue.push_back(command);
-    SDL_SemPost(enabler->async_tobox.sem);
-    SDL_SemPost(enabler->async_tobox.sem_fill);
-
-    // Wait for async_frames to change zero -> anything, either as the result of our call above
-    // or after mainloop(), or by another request. Either case means we're not in the mainloop() anymore.
-    // remote_on check is needed to avoid race condition when stopping or unloading from console,
-    // and the loop is already suspended by DFHack core (and is waiting in SDL_NumJoysticks())
-    while(!*frames && remote_on)
-    {
-#if !defined(__APPLE__) && !defined(WIN32)
-        // Looks like in some cases (e.g. ending the game), this is getting reset to false, which
-        // for some reason is causing this loop to never complete on single-core computers
-        enabler->async_paused = true;
-#endif
-#ifdef WIN32
-        __nop();
-#else
-        asm volatile ("nop");
-#endif        
-    }*/
 
     // At this point we can be sure mainloop() has finished and won't be called again
     
